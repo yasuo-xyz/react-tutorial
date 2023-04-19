61de97f687c09ce972ea44099abd6f3aee050928
dora.xyz <dora.xyz@icloud.com>
2023-04-19 21:43:48 +0900

    1 コンポーネントについて

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// コンポーネントの中身
+import { useState } from "react";
+import TodoList from "./TodoList";
 
+// コンポーネントの中にHTNL要素を追加
 function App() {
+  //　useState(フックス)とは、変数を監視、管理する物
+  // todos(変数)とはこれから追加するタスク(オブジェクト[name,id,...])
+  // 役割としてはざっくり言うとページをリロードしてくれる
+  // todosの変数が変わった時だけページを更新する事が出来るので、無駄な再レンダリングを防ぐ
+  // 要は1つの変数(2つでもいい)を監視しておいて、その変数に何かしらの更新が行われた場合ページを再レンダリングして表示をページを変える
+  const [todos, setTodos] = useState(["Todo1", "Todo2"]);
+
+  // 最初のtodosはpropsと呼ばれてる名前
+  // todosとゆう名前でコンポーネント(TodoList)に渡して下さいね、ゆう記述
+  // {}の中は渡したい変数を入れる
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
+    </>
   );
 }
 
---
+++ src/TodoList.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+// コンポーネントを定義
+const TodoList = () => {
+  return <div>TodoList</div>
+};
+
+export default TodoList
